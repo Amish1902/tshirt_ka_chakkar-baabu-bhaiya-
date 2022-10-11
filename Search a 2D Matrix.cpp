ac93fedaefc4823a5ad604cdf61d13f8bed001cf
Ananay <ananaygupta23@gmail.com>
2022-10-11 08:11:01 +0530

    add

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -1,18 +1,21 @@
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix[0].size();
-        int n=matrix.size();
-        int i=0;
-        int j=m-1;
-        while(i>=0 && i<n && j>=0 && j<m){
-            if(matrix[i][j]==target)
-                return true;
-            else if(matrix[i][j]>target)
-                j--;
-            else if(matrix[i][j]<target)
-                i++;
-        }
-        return false;
-    }
-};
+    
+	int numMatchingSubseq (string S, vector<string>& words) {
+		vector<vector<int>> alpha (26);
+		for (int i = 0; i < S.size (); ++i) alpha[S[i] - 'a'].push_back (i);
+		int res = 0;
+
+		for (const auto& word : words) {
+			int x = -1;
+			bool found = true;
+
+			for (char c : word) {
+				auto it = upper_bound (alpha[c - 'a'].begin (), alpha[c - 'a'].end (), x);
+				if (it == alpha[c - 'a'].end ()) found = false;
+				else x = *it;
+			}
+
+			if (found) res++;
+		}
+
+		return res;
+	}
--- checknetwork.py
+++ checknetwork.py
@@ -1,8 +1,1 @@
-from urllib.request import urlopen 
-
-def checkNetwork():
-    try:
-        urlopen('https://1.1.1.1', timeout=1)
-        return True
-    except urllib.error.URLError as err:
-        return False
+https://github.com/An-Yay/tshirt_ka_chakkar-baabu-bhaiya-.git
